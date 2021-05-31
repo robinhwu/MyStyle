98ec70a582adff144e2b1c19a52901e22c6e6d26
robinhwu <robinhwu@icloud.cojm>
2021-05-31 21:10:01 +0800

    Spinner

--- MyStyle/In-App-Purchase/IAPProducts.swift
+++ MyStyle/In-App-Purchase/IAPProducts.swift
@@ -10,5 +10,5 @@ import Foundation
 enum IAPProducts: String {
     case banana               = "banana"
     case pineapple            = "pineapple"
-    case watermelon          = "watermelon"
+    case watermelon           = "watermelon"
 }
--- MyStyle/In-App-Purchase/IAPService.swift
+++ MyStyle/In-App-Purchase/IAPService.swift
@@ -12,8 +12,50 @@ class IAPService: NSObject {
     private override init() {}
     static let shared = IAPService()
     
+    func showUniversalLoadingView(_ show: Bool, loadingText : String = "") {
+        let existingView = UIApplication.shared.windows[0].viewWithTag(2021)
+        if show {
+            if existingView != nil {
+                return
+            }
+            let loadingView = self.makeLoadingView(withFrame: UIScreen.main.bounds, loadingText: loadingText)
+            loadingView?.tag = 2021
+            UIApplication.shared.windows[0].addSubview(loadingView!)
+        } else {
+            existingView?.removeFromSuperview()
+        }
+
+    }
+
+     func makeLoadingView(withFrame frame: CGRect, loadingText text: String?) -> UIView? {
+        let loadingView = UIView(frame: frame)
+        loadingView.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.5)
+        let activityIndicator = UIActivityIndicatorView(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
+        //activityIndicator.backgroundColor = UIColor(red:0.16, green:0.17, blue:0.21, alpha:1)
+        activityIndicator.layer.cornerRadius = 6
+        activityIndicator.center = loadingView.center
+        activityIndicator.hidesWhenStopped = true
+        activityIndicator.style = .large
+        activityIndicator.startAnimating()
+        activityIndicator.tag = 100 // 100 for example
+
+        loadingView.addSubview(activityIndicator)
+        if !text!.isEmpty {
+            let lbl = UILabel(frame: CGRect(x: 0, y: 0, width: 200, height: 30))
+            let cpoint = CGPoint(x: activityIndicator.frame.origin.x + activityIndicator.frame.size.width / 2, y: activityIndicator.frame.origin.y + 80)
+            lbl.center = cpoint
+            lbl.textColor = UIColor.white
+            lbl.textAlignment = .center
+            lbl.text = text
+            lbl.tag = 2021
+            loadingView.addSubview(lbl)
+        }
+        return loadingView
+    }
+    
     var products = [SKProduct]()
     var paymentQueue = SKPaymentQueue.default()
+    var onBuyProductHandler: ((Result<Bool, Error>) -> Void)?
     
     func getProducts() {
         let products: Set = [IAPProducts.banana.rawValue,
@@ -36,9 +78,21 @@ class IAPService: NSObject {
         return SKPaymentQueue.canMakePayments()
     }
     
-    func purchase(product: IAPProducts) {
+    // MARK: - Purchase Products
+    
+    func buy(product: String, withHandler handler: @escaping((_ result: Result<Bool, Error>) -> Void)) {
         guard let productToPurchase =
-                products.filter({ $0.productIdentifier == product.rawValue}).first else { return }
+                products.filter({ $0.productIdentifier == product }).first else { return }
+        let payment = SKPayment(product: productToPurchase)
+        paymentQueue.add(payment)
+        
+        // Keep the completion handler.
+        onBuyProductHandler = handler
+    }
+    
+    func purchase(product: String) {
+        guard let productToPurchase =
+                products.filter({ $0.productIdentifier == product }).first else { return }
         let payment = SKPayment(product: productToPurchase)
         paymentQueue.add(payment)
     }
@@ -71,10 +125,15 @@ extension IAPService: SKPaymentTransactionObserver {
         for transaction in transactions {
             print(transaction.transactionState.statue(), transaction.payment.productIdentifier)
             switch transaction.transactionState {
-            case .purchasing: break
+            case .purchasing:
+                showUniversalLoadingView(true, loadingText: "Loading...")
             default: queue.finishTransaction(transaction)
             }
         }
+    }
+    
+    func paymentQueue(_ queue: SKPaymentQueue, removedTransactions transactions: [SKPaymentTransaction]) {
+        showUniversalLoadingView(false)
     }
 }
 
